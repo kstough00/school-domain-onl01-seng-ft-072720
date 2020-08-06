a136076c6eaba53a1238a2f73c2b076e6904621c
Kyle Stough <kstough00@gmail.com>
2020-08-06 19:28:35 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,14 +1,22 @@
 class School
-  attr_accessor :name, :roster
-  
-  def initialize(name)
-    @name = name
-    @roster = {}
-  end 
-  
-  def add_student(student, grade)
-    roster[grade] ||= []
-    roster[grade] << student
+	attr_accessor :roster, :name, :grade 
+	attr_reader :school 
+	
+	def initialize(school)
+    @school = school
   end
   
+  def roster
+  	roster = {}
+  end 
+  
+  def add_student(name,grade)
+  	roster[grade] = []
+  	roster[grade] << name
+  		
+  end
+
 end
+
+
+School.new("R").roster.add_student("john",9)
